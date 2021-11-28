59681fdce48141ea24346dc19237024f4e36e6d6
OutcastsNFT <8outcastsnft8@gmail.com>
2021-11-29 00:58:11 +0200

    Add updateMetadataVariables feature

---
+++ Outcasts-Toolbox/tools/updateMetadataVariables.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+const { exportNFTDir } = require("../main/setupWorkspace");
+const updatedVariables = require("../setup/updateMetadataVariables.json");
+
+let rawMetadata = fs.readFileSync(path.join(exportNFTDir, `_metadata.json`));
+let metadata = JSON.parse(rawMetadata);
+
+metadata.forEach((item) => {
+  Object.keys(updatedVariables).forEach((variableName_) => {
+    if (variableName_ === 'name') {
+      item[variableName_] = `${updatedVariables[variableName_]}${item.index + 1}`;
+    } else {
+      item[variableName_] = updatedVariables[variableName_];
+    }
+  })
+  fs.writeFileSync(
+    path.join(exportNFTDir, `${item.index}.json`),
+    JSON.stringify(item, null, 2)
+  );
+});
+
+fs.writeFileSync(
+  path.join(exportNFTDir, `_metadata.json`),
+  JSON.stringify(metadata, null, 2)
+);
+
+console.log(`Updated '${Object.keys(updatedVariables)}' for metadata in: ${path.join(exportNFTDir, `_metadata.json`)}`);
